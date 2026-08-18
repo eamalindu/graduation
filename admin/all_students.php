778d4e2f4aee816f8a4bc224ab373f5937e5eb84
eamalindu <eamalindu@gmail.com>
2026-08-18 12:40:38 +0530

    Add download button for exporting student data to Excel

--- admin/all_students.php
+++ admin/all_students.php
@@ -58,8 +58,9 @@ requireAdminLogin();
                     <!-- Student rows will be populated here by JavaScript -->
                     </tbody>
                 </table>
+                <button id="download-btn"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-download-icon lucide-download"><path d="M12 15V3"/><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><path d="m7 10 5 5 5-5"/></svg>&nbsp;Export Data to Excel</button>
+
         </section>
-        <button id="download-btn">Export Data to Excel</button>
 
     </main>
 </div>

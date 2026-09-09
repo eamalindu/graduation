de1c74de8cc72b4eb58f7d3d0b1af3bae15352fd
eamalindu <eamalindu@gmail.com>
2026-09-09 15:26:55 +0530

    Update header text in login page for clarity

--- admin/login.php
+++ admin/login.php
@@ -79,7 +79,7 @@ $csrfToken = csrfToken();
             <a href="">
                 <img src="../images/MC.png" alt="Metropolitan College Seal" class="seal__img" width="100" height="100"></a>
         </div>
-        <p class="header__eyebrow">Convocation &middot; Admin</p>
+        <p class="header__eyebrow">Graduation &middot; Admin Login</p>
         <h1 class="header__title">Metropolitan College</h1>
         <p class="header__subtitle">Sign in to manage attendance</p>
     </header>
